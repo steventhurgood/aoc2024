da3d89b21d4cf1b64085fa8d83a29cd7cf72bcc4
Steven Thurgood <steven.thurgood@gmail.com>
2024-12-05 21:59:24 +0000

    Deleted commented out code.

--- src/test/java/org/nihilistic/aoc/day5/PageSorterTests.java
+++ src/test/java/org/nihilistic/aoc/day5/PageSorterTests.java
@@ -19,57 +19,6 @@ public class PageSorterTests {
 
     }
 
-    /*
-     * @Test
-     * public void testPageSorter_whenSorting_sorts() {
-     * // arrange
-     * 
-     * var comparatorSpec = "10|20\n20|30\n10|30";
-     * var comparator = PageComparator.fromString(comparatorSpec);
-     * var input = "30,10\n30,20,10\n30,20";
-     * var pageSorter = new PageSorter(comparator);
-     * 
-     * String[][] expected = {
-     * { "10", "30" },
-     * { "10", "20", "30" },
-     * { "20", "30" },
-     * };
-     * 
-     * // act
-     * 
-     * var actual = pageSorter.sortLines(input).toArray();
-     * 
-     * // assert
-     * 
-     * Assert.assertArrayEquals(expected, actual);
-     * }
-     * 
-     * @Test
-     * public void testPageSorter_whenSortingLongerinput_sorts() {
-     * // arraneg
-     * var testResource =
-     * this.getClass().getResourceAsStream("/static/day5/day5_test.txt");
-     * var reader = new BufferedReader(new InputStreamReader(testResource));
-     * var input = reader.lines().collect(Collectors.joining("\n"));
-     * var parts = input.split("\n\n");
-     * var spec = parts[0];
-     * var pages = "75,47,61,53,29\n97,61,53,29,13\n75,29,13";
-     * 
-     * var comparator = PageComparator.fromString(spec);
-     * var sorter = new PageSorter(comparator);
-     * 
-     * String[][] expected = {
-     * { "75", "47", "61", "53", "29" },
-     * { "97", "61", "53", "29", "13" },
-     * { "75", "29", "13" },
-     * };
-     * 
-     * // act
-     * var actual = sorter.sortLines(pages).toArray();
-     * Assert.assertArrayEquals(expected, actual);
-     * }
-     */
-
     @Test
     public void testPageSorter_whenFilter_filters() {
         var parts = testInput.split("\n\n");
